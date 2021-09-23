14ec278d12a3c7b0d07c15c6c0a65bf7e1d23d77
DanielOiveira <danielcompositor1307@gmail.com>
2021-09-23 20:14:39 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,6 +29,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+   this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -68,6 +72,19 @@ export class ProfilePage {
       this.picture = 'data:image/png;base64,' + imageData;
       this.cameraOn = false;
     }, (error) => {});
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {})
+  }
+
+  cancel(){
+    return this.picture = null;
   }
 
 }
